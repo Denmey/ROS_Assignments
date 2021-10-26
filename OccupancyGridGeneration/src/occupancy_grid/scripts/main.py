ee044b61fedb71b027d5838b5a5145ad071fcb16
Denmey <Denmey98@gmail.com>
2021-10-26 20:25:41 +0300

    Add occupancy grid generation

--- OccupancyGridGeneration/src/occupancy_grid/scripts/main.py
+++ OccupancyGridGeneration/src/occupancy_grid/scripts/main.py
@@ -5,11 +5,14 @@ import math
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid
 
-# Occupancy grid parameters
-GRID_WIDTH = 30
-GRID_HEIGHT = 30
-GRID_RESOLUTION = 0.1
 
+MAX_FLOAT = 5000.0
+# Occupancy grid parameters
+GRID_WIDTH_UNITS = 30
+GRID_HEIGHT_UNITS = 30
+GRID_RESOLUTION = 0.1
+GRID_WIDTH_CELLS = int(GRID_WIDTH_UNITS / GRID_RESOLUTION)
+GRID_HEIGHT_CELLS = int(GRID_HEIGHT_UNITS / GRID_RESOLUTION)
 
 rospy.init_node('occupancy_grid_generator')
 rate = rospy.Rate(1)
@@ -32,7 +35,6 @@ def dist(p1, p2):
 
 def remove_outliers(ranges, angle_min, angle_step):
     OVERSTEP = 3
-    MAX_FLOAT = 5000.0
     MAX_DISTANCE = 0.05
     results = [MAX_FLOAT] * OVERSTEP
 
@@ -55,6 +57,62 @@ def remove_outliers(ranges, angle_min, angle_step):
 
     return results
 
+def init_occupancy_grid_msg():
+    occupancy_grid_msg = OccupancyGrid()
+
+    occupancy_grid_msg.header.frame_id = "base_link"
+    occupancy_grid_msg.info.width = GRID_WIDTH_CELLS
+    occupancy_grid_msg.info.height = GRID_HEIGHT_CELLS
+    occupancy_grid_msg.info.resolution = GRID_RESOLUTION
+    occupancy_grid_msg.info.origin.position.x = -GRID_WIDTH_UNITS/2.0
+    occupancy_grid_msg.info.origin.position.y = -GRID_HEIGHT_UNITS/2.0
+    occupancy_grid_msg.info.origin.position.z = 0.25
+    occupancy_grid_msg.data = [-1] * (GRID_WIDTH_CELLS * GRID_HEIGHT_CELLS)
+
+    return occupancy_grid_msg
+
+def get_cell(x, y):
+    # Origin of offset is unknown :(
+    OFFSET_X = 0.25
+    OFFSET_Y = -0.025
+
+    cell_x = round((x + OFFSET_X + GRID_WIDTH_UNITS/2) / GRID_RESOLUTION) # Translate from [-s/2, s/2] to [0, s]
+    cell_y = round((y + OFFSET_Y + GRID_HEIGHT_UNITS/2) / GRID_RESOLUTION)
+
+    return cell_x, cell_y
+
+# Sets -1 cells to 0 along the ray
+def cast_ray_and_set_free_cells(grid, dist, theta):
+    STEP = GRID_RESOLUTION / 2.0
+    STEPS = dist / STEP
+    for i in range(0, int(STEPS)):
+        r = dist / STEPS * i
+        x, y = polar2cart(r, theta)
+        cell_x, cell_y = get_cell(x, y)
+        index = cell_x + cell_y * GRID_WIDTH_CELLS
+        if grid[index] == -1:
+            grid[index] = 0
+
+def get_occupancy_grid_msg(ranges, angle_min, angle_step):
+
+    occupancy_grid_msg = init_occupancy_grid_msg()
+    
+    # occupancy_grid_msg.data[0] = 0
+    # occupancy_grid_msg.data[1] = 0
+
+    for i, distance in enumerate(ranges):
+        if distance > MAX_FLOAT-1.0:
+            continue
+        angle = angle_min+angle_step*i
+
+        # Performance heavy thing
+        cast_ray_and_set_free_cells(occupancy_grid_msg.data, distance, angle)
+
+        coords = polar2cart(distance, angle)
+        cell_x, cell_y = get_cell(coords[0], coords[1])
+        occupancy_grid_msg.data[cell_x + cell_y * GRID_WIDTH_CELLS] = 100
+
+    return occupancy_grid_msg
 
 def process_scan(msg):
     original_publisher.publish(msg)
@@ -62,20 +120,7 @@ def process_scan(msg):
     msg.ranges = remove_outliers(msg.ranges, msg.angle_min, msg.angle_increment)
     no_outliers_publisher.publish(msg)
 
-    occupancy_grid_msg = OccupancyGrid()
-
-
-    width = int(GRID_WIDTH / GRID_RESOLUTION)
-    height = int(GRID_HEIGHT / GRID_RESOLUTION)
-    occupancy_grid_msg.header.frame_id = "base_link"
-    occupancy_grid_msg.info.width = width
-    occupancy_grid_msg.info.height = height
-    occupancy_grid_msg.info.resolution = GRID_RESOLUTION
-    occupancy_grid_msg.info.origin.position.x = -GRID_WIDTH/2.0
-    occupancy_grid_msg.info.origin.position.y = -GRID_HEIGHT/2.0
-    occupancy_grid_msg.data = [-1] * (width * height)
-
-    occupancy_grid_publisher.publish(occupancy_grid_msg)
+    occupancy_grid_publisher.publish(get_occupancy_grid_msg(msg.ranges, msg.angle_min, msg.angle_increment))
 
 rospy.Subscriber('base_scan', LaserScan, process_scan)
 print("Script initialization done")
